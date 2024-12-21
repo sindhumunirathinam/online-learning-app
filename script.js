5157f00b50d994d852ddc714405c7217ab591451
Sindhu-M <142781283+sindhumunirathinam@users.noreply.github.com>
2024-12-21 11:35:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('registerForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Registration Successful!');
+  });
+  
+  document.getElementById('loginForm')?.addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Login Successful!');
+  });
